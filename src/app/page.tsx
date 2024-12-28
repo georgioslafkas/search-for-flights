fcf4cf86388040d8ecc445824938efbec2b26fba
Georgios Lafkas <georgelafkas@gmail.com>
2024-12-28 23:44:23 +0100

    Change heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ function App() {
 
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 shadow-md rounded-lg">
-      <h1 className="text-2xl font-bold text-gray-800 mb-6">Search Flights</h1>
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">Find Flights</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label
